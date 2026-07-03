c6e4e090fe723dd3eeb9acc08d2cd58158d9c56d
Suyash Gautam <gautamsuyash5@gmail.com>
2026-07-03 11:48:30 +0530

    Version 2 URL added

--- auth-config.js
+++ auth-config.js
@@ -1,5 +1,5 @@
 window.SIMTEL_AUTH_CONFIG = {
-    AUTH_SERVER_URL: 'https://plc-deploy-vuze.vercel.app',
+    AUTH_SERVER_URL: 'https://plc-version2-backend.vercel.app',
     APP_ID: 'plc-simtel',
     LOGIN_PAGE: '/login.html',      // ← must have .html
     ADMIN_PAGE: '/admin.html',      // ← must have .html
